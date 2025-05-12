a3de1df4953520853bb164ede38290b570f3eb69
Nicola Busanello <nicola.busanello@gmail.com>
2025-05-12 15:55:31 +0200

    regtest.sh improvements
     * wait for services to have started, up to a timeout
     * make bound port check cross-platform (linux/mac)
     * simplify command handling

--- regtest.sh
+++ regtest.sh
@@ -12,10 +12,55 @@ if ! $COMPOSE >/dev/null; then
 fi
 BITCOIN_CLI="$COMPOSE exec -u blits bitcoind bitcoin-cli -regtest"
 INITIAL_BLOCKS=103
+TIMEOUT=100
 
 _die () {
     echo "ERR: $*"
     exit 1
+}
+
+_is_port_bound() {
+    local port=$1
+    case "$(uname)" in
+        "Linux")
+            [ -n "$(ss -HOlnt "sport = :$port")" ] && return 0
+            ;;
+        "Darwin")
+            lsof -i "tcp:${port}" -sTCP:LISTEN -t >/dev/null && return 0
+            ;;
+        *)
+            _die "port check unsupported on this OS ($(uname))"
+            ;;
+    esac
+    return 1
+}
+
+_wait_for_bitcoind() {
+    # wait for bitcoind to be up
+    start_time=$(date +%s)
+    until $COMPOSE logs bitcoind |grep -q 'Bound to'; do
+        current_time=$(date +%s)
+        if [ $((current_time - start_time)) -gt $TIMEOUT ]; then
+            echo "Timeout waiting for bitcoind to start"
+            $COMPOSE logs bitcoind
+            exit 1
+        fi
+        sleep 1
+    done
+}
+
+_wait_for_electrs() {
+    # wait for electrs to have completed startup
+    start_time=$(date +%s)
+    until $COMPOSE logs electrs |grep -q 'finished full compaction'; do
+        current_time=$(date +%s)
+        if [ $((current_time - start_time)) -gt $TIMEOUT ]; then
+            echo "Timeout waiting for electrs to start"
+            $COMPOSE logs electrs
+            exit 1
+        fi
+        sleep 1
+    done
 }
 
 _start_services() {
@@ -25,32 +70,33 @@ _start_services() {
     # see compose.yaml for the exposed ports
     EXPOSED_PORTS=(3000 50001)
     for port in "${EXPOSED_PORTS[@]}"; do
-        if [ -n "$(ss -HOlnt "sport = :$port")" ];then
+        if _is_port_bound "$port"; then
             _die "port $port is already bound, services can't be started"
         fi
     done
     $COMPOSE up -d
     echo && echo "preparing bitcoind wallet"
+    _wait_for_bitcoind
     $BITCOIN_CLI createwallet miner >/dev/null
     $BITCOIN_CLI -rpcwallet=miner -generate $INITIAL_BLOCKS >/dev/null
-    export HEIGHT=$INITIAL_BLOCKS
-    # wait for electrs to have completed startup
-    until $COMPOSE logs electrs |grep 'finished full compaction' >/dev/null; do
-        sleep 1
-    done
+    echo "waiting for electrs to have completed startup"
+    _wait_for_electrs
 }
 
 _stop_services() {
-    $COMPOSE down --remove-orphans
+    $COMPOSE down -v --remove-orphans
     rm -rf data{core,index,ldk0,ldk1,ldk2}
 }
 
 _mine() {
-    $BITCOIN_CLI -rpcwallet=miner -generate "$NUM_BLOCKS" >/dev/null
+    local num_blocks="$1"
+    $BITCOIN_CLI -rpcwallet=miner -generate "$num_blocks" >/dev/null
 }
 
 _sendtoaddress() {
-    $BITCOIN_CLI sendtoaddress "$ADDRESS" "$AMOUNT"
+    local address="$1"
+    local amount="$2"
+    $BITCOIN_CLI sendtoaddress "$address" "$amount"
 }
 
 _help() {
@@ -59,6 +105,7 @@ _help() {
     echo
     echo "$name start"
     echo "    stop services, clean up, start services,"
+    echo "    wait for services to have completed startup,"
     echo "    create bitcoind wallet used for mining,"
     echo "    generate initial blocks"
     echo
@@ -69,54 +116,31 @@ _help() {
     echo "    mine the requested number of blocks"
     echo
     echo "$name sendtoaddress <address> <amount>"
-    echo "    send to a bitcoin address"
+    echo "    send the requested amount to the specified bitcoin address"
 }
 
 # cmdline arguments
 [ -z "$1" ] && _help
-while [ -n "$1" ]; do
-    case $1 in
-        -h|--help)
-            _help
-            exit 0
-            ;;
-        start)
-            start=1
-            ;;
-        stop)
-            stop=1
-            ;;
-        mine)
-            [ -n "$2" ] || _die "num blocks is required"
-            NUM_BLOCKS="$2"
-            mine=1
-            shift
-            ;;
-        sendtoaddress)
-            [ -n "$2" ] || _die "address is required"
-            [ -n "$3" ] || _die "amount is required"
-            ADDRESS="$2"
-            AMOUNT="$3"
-            sendtoaddress=1
-            shift 2
-            ;;
-        *)
-            _die "unsupported argument \"$1\""
-            ;;
-    esac
-    shift
-done
-
-# start services if requested
-[ "$start" = "1" ] && _start_services
-
-# stop services if requested
-if [ "$stop" = "1" ]; then
-    _stop_services
-fi
-
-[ "$mine" = "1" ] && _mine
-
-[ "$sendtoaddress" = "1" ] && _sendtoaddress
-
-exit 0
+case $1 in
+    -h|--help)
+        _help
+        ;;
+    start)
+        _start_services
+        ;;
+    stop)
+        _stop_services
+        ;;
+    mine)
+        [ -n "$2" ] || _die "num blocks is required"
+        _mine "$2"
+        ;;
+    sendtoaddress)
+        [ -n "$2" ] || _die "address is required"
+        [ -n "$3" ] || _die "amount is required"
+        _sendtoaddress "$2" "$3"
+        ;;
+    *)
+        _die "unsupported argument \"$1\""
+        ;;
+esac
